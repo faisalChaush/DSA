eb8c31cace715c35f8524cc9ea8228d19f295a6e
faisalChaush <chaushfaisal5@gmail.com>
2024-11-09 18:23:32 +0530

    Change Case

--- changeCaseOfText.js
+++ changeCaseOfText.js
@@ -3,56 +3,54 @@
 
 // Try 1
 
-// let str1="THIS IS SMALL TEXT"
-// let result=[]
+let str="THIS IS SMALL TEXT"
+let result1=[]
 
-// for(let i=0;i<str1.length;i++){
-//     if(str1[i]==str1[i].toUpperCase()){
-//       result.push(str1[i].toLowerCase())  
-//     }else if(str1[i]==str1[i].toLowerCase()){
-//      result.push(str1[i].toUpperCase())    
-//     }
-// }
-// let final=result.join('')
-// console.log(final)
+for(let i=0;i<str.length;i++){
+    if(str[i]==str[i].toUpperCase()){
+      result1.push(str[i].toLowerCase())  
+    }else if(str[i]==str[i].toLowerCase()){
+      result1.push(str[i].toUpperCase())    
+    }
+}
+let final=result1.join('')
+console.log(final)
 
 // Try 2
 
-// let str1="THIS IS SMALL TEXT"
+let str1="THIS IS SMALL TEXT"
 
-// const changeCase=()=>{
-//     let result=[]
-// for(let i=0;i<str1.length;i++){
-//     if(str1[i]==str1[i].toUpperCase()){
-//       result.push(str1[i].toLowerCase())  
-//     }else if(str1[i]==str1[i].toLowerCase()){
-//      result.push(str1[i].toUpperCase())    
-//     }
-// }
-// let final=result.join('')
+const changeCase=()=>{
+    let result=[]
+for(let i=0;i<str1.length;i++){
+    if(str1[i]==str1[i].toUpperCase()){
+      result.push(str1[i].toLowerCase())  
+    }else if(str1[i]==str1[i].toLowerCase()){
+     result.push(str1[i].toUpperCase())    
+    }
+}
+let final=result.join('')
 
-// return final
-// }
-// console.log(changeCase(str1))
+return final
+}
+console.log(changeCase(str1))
 
 
 
 // Try 3
 
-const changeCase = (str)=>{
+const changeCase2 = (str)=>{
     let newStr =''
     for(i=0;i<str.length;i++){
         if(str[i]==str[i].toUpperCase()){
           newStr+=str[i].toLowerCase()
-        
         }else{
-          newStr+= str[i].toUpperCase()
-             
+          newStr+= str[i].toUpperCase()    
         }
     }
     return newStr
     
 }
 
-let result = changeCase("faisal")
+let result = changeCase2("faisal")
 console.log(result)
